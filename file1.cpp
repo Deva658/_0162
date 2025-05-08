b15deb33bd1d8303e9348737b90452f0c58dc4a1
Deva658 <devaaditya271@gmail.com>
2025-05-08 10:54:15 +0700

    menambahkan fungsi delNode untuk penghapusan node

--- file1.cpp
+++ file1.cpp
@@ -70,4 +70,19 @@ class list {
 
         return (*current != NULL);
     }
+
+    bool delNode(int nim)
+    {
+        Node *current, *previous;
+        if (!Search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+        
+        delete current;
+        return true;
+    }
 }
