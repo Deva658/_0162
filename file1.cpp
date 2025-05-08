6b467502fbc6baf9eb553f3806530f44a8e4146a
Deva658 <devaaditya271@gmail.com>
2025-05-08 11:01:15 +0700

    menambahkan fungsi traverse untuk menampilkan isi list

--- file1.cpp
+++ file1.cpp
@@ -85,4 +85,23 @@ class list {
         delete current;
         return true;
     }
+
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList Kosong\n"
+        }
+        else
+        {
+            cout << "\nData didalam list adalah\n";
+            Node *currentNode = START;
+            while (currentNode != NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
 }
